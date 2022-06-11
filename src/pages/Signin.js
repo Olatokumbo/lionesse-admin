d62252ba733717ea434b9aed1a3f05395dc6b27f
David Faith <faithodesola@gmail.com>
2022-06-11 11:46:05 +0100

    Linked up Signin logic

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -12,6 +12,7 @@ import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { signin } from "../firebase/auth";
 
 function Copyright(props) {
   return (
@@ -34,13 +35,13 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function SignIn() {
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+    const email = data.get("email");
+    const password = data.get("password");
+
+    await signin(email, password);
   };
 
   return (
