cb71adba1ca216f82c2c5f54fc8695340d54cc9d
David Faith <faithodesola@gmail.com>
2022-06-18 23:01:42 +0100

    Add data to user  page

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,11 +1,44 @@
-import { Avatar, Grid, Typography, Box } from "@mui/material";
+import { Avatar, Grid, Typography, Box, Paper } from "@mui/material";
+import { useEffect, useState } from "react";
+import { getUserById } from "../firebase/user";
+import { useParams } from "react-router-dom";
 
 const User = () => {
+  const params = useParams();
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getUserById(params.userId);
+      setUser(data);
+    };
+
+    getData();
+  }, [params.userId]);
   return (
     <Grid>
-      <Typography>User Info</Typography>
-      <Box>
-        <Avatar size="5rem">h</Avatar>
+      <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+        User Info
+      </Typography>
+      <Box sx={{ display: "flex", flexDirection: "column", padding: "5rem" }}>
+        <Paper
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "2rem",
+          }}
+        >
+          <Avatar
+            sx={{ width: 100, height: 100 }}
+            src={`https://picsum.photos/id/10/2500/1667`}
+          />
+          <Typography variant="h4" sx={{ fontWeight: "bold", marginY: "1rem" }}>
+            {user ? `${user.firstName} ${user.lastName}`: "Loading..."}
+          </Typography>
+          <Typography>Email: {user ? `${user.email}`: "Loading..."}</Typography>
+          <Typography>Role: {user ? `${user.role}`: "Loading..."}</Typography>
+        </Paper>
       </Box>
     </Grid>
   );
